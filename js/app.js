fc89c62f0fcbf49ce812884793b0f2f734914c33
Eduar Trujillo <eetgrisales@gmail.com>
2023-11-10 10:28:00 -0500

    Add validation to insurance form, Added required attributes to the brand, year, and type input fields, Updated the quoteInsurance() function to check for empty fields and display an error message if any are found

--- js/app.js
+++ js/app.js
@@ -8,20 +8,41 @@ function UI() {}
 
 UI.prototype.fillOptions = () => {
 	const max = new Date().getFullYear(),
-		min = max - 20;
+		min = max - 23;
 
-    const selectYear = document.querySelector('#year');
+	const selectYear = document.querySelector('#year');
 
-    for (let i = max; i >= min; i--) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i;
-        selectYear.appendChild(option);
-    }
+	for (let i = max; i >= min; i--) {
+		const option = document.createElement('option');
+		option.value = i;
+		option.textContent = i;
+		selectYear.appendChild(option);
+	}
 };
 
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', () => {
-    ui.fillOptions();
+	ui.fillOptions();
 });
+
+eventListeners();
+
+function eventListeners() {
+	const form = document.querySelector('#insurance-quote');
+	form.addEventListener('submit', quoteInsurance);
+}
+
+function quoteInsurance(event) {
+	event.preventDefault();
+
+	const brand = document.querySelector('#brand').value;
+	const year = document.querySelector('#year').value;
+	const type = document.querySelector('input[name="type"]:checked').value;
+
+	if (brand === '' || year === '' || type === '') {
+		console.log('Error');
+	} else {
+		console.log('Quote...');
+	}
+}
